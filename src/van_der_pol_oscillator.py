106021d65652b3b79dad90e90167b8c925d88d7e
Veit Hucke <v-h@mailbox.org>
2025-01-06 12:48:32 +0100

    Started implementation of sliders to interactively change initial conditions for ODE solver.

--- src/van_der_pol_oscillator.py
+++ src/van_der_pol_oscillator.py
@@ -1,42 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
 
 # Define global variables
-CARRYING_CAPACITY = 100
-BIRTH_RATE = 1
-N_0 = 1
-DELTA_H = 0.05
-T_0 = 0
-T_END = 15
-TIME_ARRAY = np.linspace(T_0, T_END, 100)
-X_0 = 1
-Y_0 = 1
-
-
-def function_n(t):
-    """Analytical solution to the Logistig Growth ODE.
-
-    Args:
-        t (numpy.ndarray): List of discrete time points at which the equation is to be evaluated.
-
-    Returns:
-        numpy.ndarray: Function values at time points t.
-    """
-
-    n_t = (N_0 * CARRYING_CAPACITY * np.exp(BIRTH_RATE * t)) / (CARRYING_CAPACITY + N_0 * (np.exp(BIRTH_RATE * t) - 1))
-    return n_t
-
-def n_prime(n_t):
-    """Differential equation for logistic growth
-
-    Args:
-        n_t (float): Number of individuals at time t.
-
-    Returns:
-        float: dn/dt
-    """
-
-    return BIRTH_RATE * n_t * (1 - n_t/CARRYING_CAPACITY)
+T_END = 25
 
 def runge_kutta(function, t_0, x_0, y_0, d_h, t_end=T_END):
     """Runge-Kutta 4 method to integrate an ODE.
@@ -44,7 +11,6 @@ def runge_kutta(function, t_0, x_0, y_0, d_h, t_end=T_END):
     Args:
         function (_type_): _description_
         t_0 (float): Starting time. Lower bound of integration.
-        n_0 (float): Starting number of N. 
         d_h (float): Integration interval. Step width.
         t_end (float, optional): End time. Upper bound of integration. Defaults to T_END.
 
@@ -79,34 +45,6 @@ def runge_kutta(function, t_0, x_0, y_0, d_h, t_end=T_END):
 
     return steps, x_approx[:-1], y_approx[:-1]
 
-def runge_kutta_alt(function, t_0, n_0, d_h, t_end=T_END):
-    """Runge-Kutta 4 method to integrate an ODE.
-
-    Args:
-        function (_type_): _description_
-        t_0 (float): Starting time. Lower bound of integration.
-        n_0 (float): Starting number of N. 
-        d_h (float): Integration interval. Step width.
-        t_end (float, optional): End time. Upper bound of integration. Defaults to T_END.
-
-    Returns:
-        tuple: Time points and approximated function value at time points t.
-    """
-    n = n_0
-    steps = np.arange(t_0, t_end + d_h, d_h)
-    n_approx = [n]
-    for t in steps:
-        k1 = function(n)
-        k2 = function(n + k1 * d_h/2)
-        k3 = function(n + k2 * d_h/2)
-        k4 = function(n + k3 * d_h)
-        k_array = np.array([k1, k2, k3, k4])
-        weights = np.array([1/6, 1/3, 1/3, 1/6])
-        n += d_h * np.dot(weights, k_array)
-        n_approx.append(n)
-    return steps, n_approx[:-1]
-
-# TODO: Somehow make parameter mu variable.
 def vdp_prime(x, y, mu=2.0):
     """Van der Pol oscillator. System of two interacting equations.
 
@@ -119,46 +57,108 @@ def vdp_prime(x, y, mu=2.0):
         tuple: Rates of change (dx/dt, dy/dt).
     """
 
-    F_x = 1/3 * x**3 - x
+    F_x = (1/3) * x**3 - x
     x_prime = mu * (y - F_x)
     y_prime = -(1/mu)*x
     return x_prime, y_prime
 
+def F_x_function(x):
+    """Function of the y nullcline of the Van der Pol oscillator.
+
+    Args:
+        x (numpy.ndarray): Input values to the function f(x).
+
+    Returns:
+        numpy.ndarray: Function values evaluated at x.
+    """
+    
+    return (1/3) * x**3 - x
+
 if __name__ == '__main__':
-
-    # Analytical solution to the ODE
-    N_T = function_n(TIME_ARRAY)
-
-    # Runge-Kutta 4 solution to the ODE
-    rk4_steps, N_t_rk4 = runge_kutta_alt(lambda n: n_prime(n), T_0, N_0, DELTA_H)
-
-    # Plot
-    """
-    fig = plt.figure(figsize=(10, 10))
-    axs = fig.add_subplot(1, 1, 1)
-    axs.plot(TIME_ARRAY, N_T, c='k', linewidth=1.0, label='Analytic')
-    axs.scatter(rk4_steps, N_t_rk4, c='m', s=4, label='Runge-Kutta')
-    axs.set_xlabel('t')
-    axs.set_ylabel('N(t)')
-    axs.set_title('Logistic Growth')
-    axs.legend()
-    axs.grid()
-    plt.show()
-    """
 
     # Initial conditions
     X_0 = 0.0
     Y_0 = 0.0
+    T_0 = 0
+    DELTA_H = 0.01
+    _mu_0 = 20.0
 
     # Now try that with the Van der Pol oscillator
-    rk4_vdp_steps, x_t_vdp, y_t_vdp = runge_kutta(lambda x, y: vdp_prime(x, y, mu=2.0), T_0, X_0, Y_0, DELTA_H, t_end=100)
+    rk4_vdp_steps, x_t_vdp, y_t_vdp = runge_kutta(lambda x, y: vdp_prime(x, y, mu=_mu_0), T_0, X_0, Y_0, DELTA_H, t_end=25)
+    """
+    # Initial conditions
+    X_0 = 8.0
+    Y_0 = 5.0
 
-    # Plot
+    # Now try that with the Van der Pol oscillator
+    rk4_vdp_steps_2, x_t_vdp_2, y_t_vdp_2 = runge_kutta(lambda x, y: vdp_prime(x, y, mu=_mu_0), T_0, X_0, Y_0, DELTA_H, t_end=25)
+
+    # Initial conditions
+    X_0 = -7.0
+    Y_0 = -4.0
+
+    # Now try that with the Van der Pol oscillator
+    rk4_vdp_steps_3, x_t_vdp_3, y_t_vdp_3 = runge_kutta(lambda x, y: vdp_prime(x, y, mu=_mu_0), T_0, X_0, Y_0, DELTA_H, t_end=25)"""
+
+    # Get the y nullcline values
+    x_nullcline = np.linspace(-7.0, 8.0, 100)
+    y_nullcline = F_x_function(x_nullcline)
+
+    # Plot dynamics in phase space
     fig = plt.figure(figsize=(10, 10))
     axs = fig.add_subplot(1, 1, 1)
-    axs.plot(x_t_vdp, y_t_vdp, c='m')
+    fig.subplots_adjust(bottom=0.25)    # Adjust the subplots region to leave some space for the sliders and buttons
+    [line] = axs.plot(x_t_vdp, y_t_vdp, c='k', linewidth=0.75)  # 'line' variable is used for modifying the line later
+    # axs.plot(x_t_vdp_2, y_t_vdp_2, c='c', linewidth=0.75)
+    # axs.plot(x_t_vdp_3, y_t_vdp_3, c='r', linewidth=0.75)
+    # axs.scatter(x_nullcline, y_nullcline, c='k', s=4)   # Plot y nullcline
     axs.set_xlabel('x(t)')
     axs.set_ylabel('y(t)')
     axs.set_title('Van der Pol oscillator')
+    axs.set_xlim([-10, 10])
+    axs.set_ylim([-10, 10])
     axs.grid()
+    
+    ## Def everything necessray for sliders and buttons
+    # Slider for dampening parameter mu
+    mu_slider_ax = fig.add_axes([0.2, 0.15, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+    mu_slider = Slider(mu_slider_ax, 'mu', valmin=0.1, valmax=50.0, valinit=_mu_0)
+    
+    # Slider for x-coordinate
+    x_slider_ax = fig.add_axes([0.2, 0.125, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+    x_slider = Slider(x_slider_ax, 'x', valmin=-10.0, valmax=10.0, valinit=X_0)
+    
+    # Slider for y-cooordinate
+    y_slider_ax = fig.add_axes([0.2, 0.1, 0.65, 0.03], facecolor='lightgoldenrodyellow')
+    y_slider = Slider(y_slider_ax, 'y', valmin=-10.0, valmax=10.0, valinit=Y_0)
+    
+    # Reset button
+    reset_button_ax = fig.add_axes([0.8, 0.025, 0.1, 0.04])
+    reset_button = Button(reset_button_ax, 'Reset', color='lightgoldenrodyellow', hovercolor='0.975')
+
+    # Define an action for modifying the line when any slider's value changes
+    def slider_on_changed(val):
+        """Changes the dampening parameter mu, x(0) and/or y(0) and reevaluates the function at that point."""
+        
+        # Evaluate differential equation with new parameter
+        _, x_t_vdp, y_t_vdp = runge_kutta(lambda x, y: vdp_prime(x=x_slider.val, y=y_slider.val, mu=mu_slider.val), T_0, X_0, Y_0, DELTA_H, t_end=25)
+        
+        # Set the new y-values
+        line.set_ydata(y_t_vdp)
+        line.set_xdata(x_t_vdp)
+        fig.canvas.draw_idle()
+    
+    # Add a button for resetting the parameters
+    def reset_button_on_clicked(mouse_event):
+        """Resets the parameters to initial values."""
+        mu_slider.reset()
+        x_slider.reset()
+        y_slider.reset()
+    
+    # Manage actions
+    mu_slider.on_changed(slider_on_changed)
+    x_slider.on_changed(slider_on_changed)
+    y_slider.on_changed(slider_on_changed)
+    reset_button.on_clicked(reset_button_on_clicked)
+        
     plt.show()
